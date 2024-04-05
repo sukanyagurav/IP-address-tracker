f68c77609d4ef77fff2d739bfcb24af377303955
sukanya gurav <sukanyagurav6@gmail.com>
2024-04-05 23:12:51 +0530

    completed project

---
+++ README.md
@@ -0,0 +1,36 @@
+# Frontend Mentor - IP address tracker
+
+![Design preview for the IP address tracker coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+
+## The challenge
+
+This challenge is to build out this IP Address Tracker app and get it looking as close to the design as possible. To get the IP Address locations, I used [IP Geolocation API by ipgeolocation](https://ipgeolocation.io/). To generate the map, I used [LeafletJS](https://leafletjs.com/).
+
+Your users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Built with
+
+- HTML
+- CSS
+- Javascript
+- LeafletJS
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+**Have fun building!** 🚀
---
+++ script.js
@@ -0,0 +1,112 @@
+import API_KEY from "./API_KEY.js"
+console.log(API_KEY)
+let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=`
+let latitude,longitude
+const ipAddressLabel= document.querySelector('.ip_address')
+const locationLabel= document.querySelector('.location')
+const timezoneLabel= document.querySelector('.timezone')
+const ispLabel= document.querySelector('.isp')
+var map = L.map('map').setView([51.505, -0.09], 19).fitWorld();
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
+
+const ipInput = document.getElementById('ip__input')
+const form = document.getElementById('form')
+const error = document.querySelector('.error')
+
+ipInput.addEventListener('input',function(e){
+    ipFormat(e.target.value)
+})
+form.addEventListener('submit',async function(e){
+    e.preventDefault()
+    let parts=[]
+    let flag = false
+    parts = ipInput.value.split('.');
+    if(ipInput.value == ''){
+        error.innerHTML=`Please enter correct IP address`
+        return
+    }
+    for (let i = 0; i < parts.length; i++) {
+        if (parts[i].length < 4) {
+            flag=false
+        }
+        else{
+            flag=true
+        }
+    }
+    if(flag){
+        error.innerHTML=`Please enter correct IP address`
+    }
+    else{
+        error.innerHTML=''
+        const data =await fetchIP(ipInput.value)
+        drawMap(data)
+        
+    }
+
+})
+
+
+window.addEventListener('load',async function(){
+    const data =await fetchIP(geoplugin_request())
+    drawMap(data)
+})
+function drawMap(data){
+    ipAddressLabel.innerHTML=data.ip
+    ipAddressLabel.classList.add('textAnimation')
+    timezoneLabel.classList.add('textAnimation')
+    ispLabel.classList.add('textAnimation')
+    locationLabel.classList.add('textAnimation')
+    timezoneLabel.innerHTML=data.time_zone.name
+    ispLabel.innerHTML=data.isp
+    locationLabel.innerHTML=`${data.city}, ${data.country_name}`
+    console.log(data.ip)
+    map.setView([data.latitude, data.longitude], 19)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: false,
+    }).addTo(map)
+   
+    map.setView([data.latitude, data.longitude],18)
+    L.marker([data.latitude,  data.longitude]).addTo(map);
+    setTimeout(()=>{
+        ipAddressLabel.classList.remove('textAnimation')
+        timezoneLabel.classList.remove('textAnimation')
+        ispLabel.classList.remove('textAnimation')
+        locationLabel.classList.remove('textAnimation')
+    },1000)
+}
+
+async function fetchIP(ipaddress){
+    try{
+        const res = await fetch(`${url}${ipaddress}`)
+        const data =await res.json()
+        if(res.status>400){
+            throw error()
+        }
+       return data
+    }
+    catch(err){
+        error.innerHTML=`Please enter correct IP address`
+    }
+}
+function ipFormat(val){
+    val = val.replace(/[^0-9.]{0,3}/g, "");
+    let dotCount = (ipInput.value.match(/\./g) || []).length;
+    
+    if (dotCount > 3 || val.includes('..')) {
+     
+        ipInput.value = ipInput.value.substring(0, ipInput.value.lastIndexOf('.'));
+        return
+    }
+    let parts=[]
+    parts = val.split('.');
+    for (let i = 0; i < parts.length; i++) {
+        if (parts[i].length > 3) {
+            parts[i] = parts[i].substring(0, 3);
+
+            return
+        }
+    }
+    ipInput.value = parts.join('.');
+}
